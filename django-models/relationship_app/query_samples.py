af889d0f77795188dff8452fe8429e51fd33c032
obum <obumanichebe@gmail.com>
2024-11-08 19:40:11 +0100

    Added quey

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
-from relationship_app.models import Book, Library, Librarian
+from relationship_app.models import Author, Book, Library, Librarian
 
-# Checks for “List all books in a library.” task
+#  “List all books in a library.” task
 
 # Assuming library_name
 library_name = "Alor library"
@@ -14,3 +14,17 @@ books_in_library = library.books.all()
 # Display the book titles
 for title in books_in_library:
     print(title)
+
+
+#  “Query all books by a specific author.” task
+
+author_name = 'Chinwe Achebe'
+
+author = Author.objects.get(name=author_name)
+
+all_books = Book.objects.all()
+
+all_book_by_author =  all_books.filter(author=author)
+
+for book in all_book_by_author:
+    print(book)
