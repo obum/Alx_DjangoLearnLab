144a14247f8b4e2f85c114aed47018f43d5abd2e
obum <obumanichebe@gmail.com>
2024-11-08 15:54:15 +0100

    modified return statement for Autor model

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     
     def __str__(self):
-        return f'{self.name}'
+        return self.name
     
 class Book(models.Model):
     title = models.CharField(max_length=50)
