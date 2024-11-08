022264095514323715ab6ea0b2ba8bcd2757d225
obum <obumanichebe@gmail.com>
2024-11-08 19:28:56 +0100

    Added quey

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,14 +2,14 @@ from relationship_app.models import Book, Library, Librarian
 
 # Checks for “List all books in a library.” task
 
-# Assuming library_id = 1
-library_id = 1
+# Assuming library_name
+library_name = "Alor library"
 
 # Get the library object
-library = Library.objects.get(id=library_id)
+library = Library.objects.get(name=library_name)
 
 # Fetch the titles of books in the specified library
-books_in_library = Book.objects.all().filter(ibraries_found=library)
+books_in_library = library.books.all()
 
 # Display the book titles
 for title in books_in_library:
