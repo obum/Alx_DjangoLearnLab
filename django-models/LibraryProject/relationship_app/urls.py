c3e9f8ffa474c499d55d32b726aff273595ba8ef
obum <obumanichebe@gmail.com>
2024-11-09 13:58:27 +0100

    Added login Logout and Registeration functionanlity

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, CustomLoginView
+
+from .views import RegisterView
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path('register/', view=RegisterView.as_view() , name='register'),
+    path('login/', view=CustomLoginView.as_view() , name='login'),
+    path('logout/', view=LogoutView.as_view() , name='logout'),
+
+
     path('', view=list_books , name='books-view'),
     path('library/<int:pk>/', view=LibraryDetailView.as_view() , name='library'),
+
 ]   
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,28 @@ from .models import Book
 
 from django.views.generic.detail import DetailView
 
-library_name = "Alor library"
+# ----This import are used for user registration ---- #
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Get the library object
+from django.contrib.auth.views import LoginView
+
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+class CustomLoginView(LoginView):
+    template_name= 'relationship_app/login.html'
+    fields = '__all__'
+    # redirect_authenticated_user = True
+    
+    
+    def get_success_url(self):
+        return reverse_lazy('books-view')
 
 
 # Create your views here.
