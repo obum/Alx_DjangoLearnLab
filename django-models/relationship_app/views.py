b13665f647330d0dfdaa7c94d776001bbe17a590
obum <obumanichebe@gmail.com>
2024-11-11 13:27:30 +0100

    Updated the code

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -16,9 +16,6 @@ from django.contrib.auth import login
 
 from django.contrib.auth.views import LoginView
 
-from .admin_view import is_admin
-from .librarian_view import is_librarian
-from .member_view import is_member
 from django.contrib.auth.decorators import user_passes_test
 
 
@@ -90,15 +87,23 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
 
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_member)
 def member_view(request):
