0be17480b306fded9c5f8c8cb24ac3a03388fd4c
obum <obumanichebe@gmail.com>
2024-11-20 16:12:03 +0100

    Added the viewset code

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.db import router
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Route for the BookList view
+    path('', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,17 @@ import rest_framework
 # from rest_framework.generics import ListAPIView
 from .serializers import BookSerializers
 from api.models import Book
+from rest_framework import viewsets
 # Create your views here.
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    
+    
+# Using Viewsets to simplify view creation of CRUD operation
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializers
+    queryset = Book.objects.all()
     
