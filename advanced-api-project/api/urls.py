6ead17fdbde8daffe3c35f70597b0fa487aa52e8
obum <obumanichebe@gmail.com>
2024-11-28 15:44:29 +0100

    Added custom views for create  and update

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list' ),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail' ),
-    path('books/add/', BookCreate.as_view(), name='book-create' ),
+    path('books/create/', BookCreate.as_view(), name='book-create' ),
     path('books/update/<int:pk>/', BookUpdate.as_view(), name='book-update' ),
     path('books/delete/<int:pk>/', BookDelete.as_view(), name='book-delete' ),
 
