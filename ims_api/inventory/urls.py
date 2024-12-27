1afd57f3284baff1f49480aa666c5462b00fb3bb
Obum <obumanichebe@gmail.com>
2024-12-27 19:49:58 +0100

    feat: Added current Quantity available endpoint

--- ims_api/inventory/urls.py
+++ ims_api/inventory/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryView, InventoryItemViewset
+from .views import CategoryView, InventoryItemViewset, InventoryLevelView
 
 router = DefaultRouter()
 router.register(r'category', CategoryView, basename='category')
 router.register(r'', InventoryItemViewset, basename='inventory')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('list/', InventoryLevelView.as_view(), name='inventory-level'),
+
+    path('', include(router.urls)),
 ]
--- ims_api/inventory/views.py
+++ ims_api/inventory/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CategorySerializer, InventoryItemSerializer
 from .models import Category, InventoryItem
-from rest_framework import permissions
+from rest_framework import permissions, generics, status
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .permissions import IsOwnerorReadonly
 # Create your views here.
@@ -20,4 +21,30 @@ class InventoryItemViewset(ModelViewSet):
     queryset = InventoryItem.objects.all()
     # Only authenticated users should be able to manage inventory (i.e., create, update, or delete items)
     permission_classes = [permissions.IsAuthenticated, IsOwnerorReadonly]
+
+class InventoryLevelView(generics.ListAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    serializer_class = InventoryItemSerializer
+    queryset = InventoryItem.objects.all()
+    
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = InventoryItemSerializer(queryset, many=True)
+        items = serializer.data
+  
+        inventory_level = [
+            {
+                "id": item['id'],
+                "name": item["name"],
+                "quantity": item["quantity"]
+            }
+            for item in items
+        ]
+        
+        print(inventory_level)
+        return Response(
+                inventory_level,
+                status=status.HTTP_200_OK
+            )
+        
 
